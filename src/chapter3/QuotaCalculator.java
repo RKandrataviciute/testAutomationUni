8e07a692a892af9169d7501f509f7e5373babbd8
Ruta Kandrataviciute <r.kandrataviciute@gmail.com>
2021-04-30 11:34:57 -0700

    Chapter 3 - IF ELSE statement

---
+++ src/chapter3/QuotaCalculator.java
@@ -0,0 +1,25 @@
+package chapter3;
+import java.util.Scanner;
+
+public class QuotaCalculator {
+
+    public static void main(String[] args) {
+
+        int quota = 10;
+
+        //Input for number of sales
+        System.out.println("Enter the number of sales you made this week:");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+        //Did employee met the quota?
+        if (sales>=quota)
+            System.out.println("Congrats! You've met your quota");
+        else {
+            int salesShort = quota-sales;
+            System.out.println("You did not make your quota. You were " + salesShort + " sales short.");
+        }
+
+    }
+}
+
